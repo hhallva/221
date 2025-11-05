3939393d936d197292eac481bda93b46fc31b6bf
221 <221@PC-Klass.ru>
2025-11-05 11:37:28 +0300

    8

---
+++ 4/ОКФКС/ЛР8/LabWork8/LabWork8/Program.cs
@@ -0,0 +1,26 @@
+﻿using System.Xml.Linq;
+
+internal class Program
+{
+
+    private static readonly string PASSWORD = "SuperSecret123";
+    private static void Main(string[] args)
+    {
+        var obj = new MyClass.Class();
+        Console.WriteLine(obj.SomeMethod());
+
+        Console.WriteLine("Введите пароль:");
+        string input = Console.ReadLine();
+
+        if (input == PASSWORD)
+        {
+            Console.WriteLine("Авторизация успешна!");
+        }
+        else
+        {
+            Console.WriteLine("Неверный пароль.");
+        }
+
+        Console.ReadKey();
+    }
+}
